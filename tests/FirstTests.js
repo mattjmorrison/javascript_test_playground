b526a0688b7d70b95c33c3db72e740d02ab611e8
Matthew J Morrison <matt.morrison@theimtgroup.com>
2010-12-13 08:25:46 -0600

    added a few simple tests using JsTestDriver

---
+++ tests/FirstTests.js
@@ -0,0 +1,14 @@
+TestCase("FirstTests", {
+
+    testOne: function(){
+        assertEquals(3, 1 + 2);
+    },
+
+    testTwo: function(){
+        assertEquals(1, 2 - 1);
+    },
+
+    testThree: function(){
+        assertEquals(4, 2 + 2);
+    }
+});
